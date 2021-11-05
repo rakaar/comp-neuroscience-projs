27017b96a04e8d701241f09dde0668f2439918f6
rakaar <rka87338@gmail.com>
2021-11-05 18:53:56 +0530

    q1

--- proj3.m
+++ proj3.m
@@ -1,3 +1,17 @@
 function project
-    data = load('data_cn_project_iii_a17.mat');
+    stimulus = load('data_cn_project_iii_a17.mat', 'Stimulus').Stimulus;
+
+    % Question - 1
+    for tau=-50:50
+      autocorr = 0;
+      for i=1:20000
+        if (i + tau >=1) & (i + tau <=20000)    
+            autocorr = autocorr + (stimulus(1,i)*stimulus(1,i+tau));
+            autocorr = autocorr/20000; % m value = 20000
+
+        end
+      end
+
+      fprintf("for tau = %f, autocorr of stimulus is %f \n", tau, autocorr);
+    end
 end
