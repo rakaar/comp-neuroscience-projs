5734acab243e933a27781d137f27cf9a1a1bcc62
rakaar <rka87338@gmail.com>
2021-11-06 12:00:23 +0530

    q2

--- proj3.m
+++ proj3.m
@@ -1,6 +1,9 @@
 function project
     stimulus = load('data_cn_project_iii_a17.mat', 'Stimulus').Stimulus;
 
+   
+    return % khatam
+   
     % Question - 1
     for tau=-50:50
       autocorr = 0;
@@ -14,4 +17,34 @@ function project
 
       fprintf("for tau = %f, autocorr of stimulus is %f \n", tau, autocorr);
     end
+
+     % Question-2
+     all_spike_times = load('data_cn_project_iii_a17.mat', 'All_Spike_Times').All_Spike_Times;
+   
+
+     for neuron_num=1:4
+         neuron_stimulus_rate = zeros(20000,1);
+         for i=1:50
+             spike_times = all_spike_times{neuron_num,i};
+             len_of_spike_array = size(spike_times,2);
+             for s=1:len_of_spike_array
+                 % spike_times(s)*1000 as per this time, depends index of addition
+                 index = fix(spike_times(s)*1000)+1;
+                 neuron_stimulus_rate(index) = neuron_stimulus_rate(index) + 1;
+ 
+             end
+         end
+ 
+         neuron_stimulus_rate = neuron_stimulus_rate./(50*0.001);
+         time_axis = transpose(linspace(0,20,20000));
+         
+         disp(min(neuron_stimulus_rate));
+         disp(max(neuron_stimulus_rate));
+ 
+         figure(neuron_num)
+             plot(time_axis, neuron_stimulus_rate);
+         grid
+ 
+     end
+   
 end
