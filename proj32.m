197abbc85eb88d3db180733412dbdc319293c965
rakaar <rka87338@gmail.com>
2021-11-15 22:50:08 +0530

    fit and plot

--- proj32.m
+++ proj32.m
@@ -75,37 +75,92 @@ function project
     for i=1:4
         y_t1(i, 1:15000) = y_t(i,1:15000);
     end
-        % average the results in bins
-        bin_size = 30;
-        for i = 1:ceil(15000/bin_size)
-            e = i*bin_size;
-            if e>15000
-                e = 15000;
-            end
-            x1(i) = mean( y_t(1, (1+(i-1)*bin_size):e) );
-            x2(i) = mean( y_t(2, (1+(i-1)*bin_size):e) );
-            x3(i) = mean( y_t(3, (1+(i-1)*bin_size):e) );
-            x4(i) = mean( y_t(4, (1+(i-1)*bin_size):e) );
-        
-            y1(i) = mean( PSTH(1, (1+(i-1)*bin_size):e) );
-            y2(i) = mean( PSTH(2, (1+(i-1)*bin_size):e) );
-            y3(i) = mean( PSTH(3, (1+(i-1)*bin_size):e) );
-            y4(i) = mean( PSTH(4, (1+(i-1)*bin_size):e) );
+    % average the results in bins
+    bin_size = 30;
+    for i = 1:ceil(15000/bin_size)
+        e = i*bin_size;
+        if e>15000
+            e = 15000;
         end
+        x1(i) = mean( y_t(1, (1+(i-1)*bin_size):e) );
+        x2(i) = mean( y_t(2, (1+(i-1)*bin_size):e) );
+        x3(i) = mean( y_t(3, (1+(i-1)*bin_size):e) );
+        x4(i) = mean( y_t(4, (1+(i-1)*bin_size):e) );
     
+        y1(i) = mean( PSTH(1, (1+(i-1)*bin_size):e) );
+        y2(i) = mean( PSTH(2, (1+(i-1)*bin_size):e) );
+        y3(i) = mean( PSTH(3, (1+(i-1)*bin_size):e) );
+        y4(i) = mean( PSTH(4, (1+(i-1)*bin_size):e) );
+    end
+    
+    % xi = h(t) - 1 x 15000, yi = PSTHi - 1 x 15000
+    
+
     figure(51)
-        scatter(x1,y1);
-    grid
-    figure(52)
-        scatter(x2,y2);
-    grid
-    figure(53)
-        scatter(x2,y2);
-    grid
-    figure(54)
-        scatter(x2,y2);
+        hold on
+        [x1, y1] = prepareCurveData( x1, y1 );
+
+        ft = fittype( 'a/(1+exp(-b*(x-c)))', 'independent', 'x', 'dependent', 'y' );
+        opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+        opts.Display = 'Off';
+        opts.StartPoint = [0.5 0.5, 0.5];
+
+        [fitresult, gof] = fit( x1, y1, ft, opts );
+        plot(fitresult,x1, y1);
+        hold off
     grid
 
+
+    figure(52)
+        hold on
+        [x2, y2] = prepareCurveData( x2, y2 );
+
+        ft = fittype( 'a/(1+exp(-b*(x-c)))', 'independent', 'x', 'dependent', 'y' );
+        opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+        opts.Display = 'Off';
+        opts.StartPoint = [0.5 0.5, 0.5];
+
+       
+        [fitresult, gof] = fit( x2, y2, ft, opts );
+        plot(fitresult,x2, y2);
+        hold off
+    grid
+
+
+    figure(53)
+        hold on
+        [x3, y3] = prepareCurveData( x3, y3 );
+
+        ft = fittype( 'a/(1+exp(-b*(x-c)))', 'independent', 'x', 'dependent', 'y' );
+        opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+        opts.Display = 'Off';
+        opts.StartPoint = [0.5 0.5, 0.5];
+
+       
+        [fitresult, gof] = fit( x3, y3, ft, opts );
+        plot(fitresult,x3, y3);
+        hold off
+    grid
+
+
+    figure(54)
+        hold on
+        [x4, y4] = prepareCurveData( x4, y4 );
+
+        ft = fittype( 'a/(1+exp(-b*(x-c)))', 'independent', 'x', 'dependent', 'y' );
+        opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+        opts.Display = 'Off';
+        opts.StartPoint = [0.5 0.5, 0.5];
+
+
+        [fitresult, gof] = fit( x4, y4, ft, opts );
+        plot(fitresult,x4, y4);
+        hold off
+    grid
+
+
+
+   
 
     
     
